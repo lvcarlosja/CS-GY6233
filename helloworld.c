c1303d846b0c5be023d06744547ef606b47f734a
lvcarlosja <62188016+lvcarlosja@users.noreply.github.com>
2020-06-09 09:15:25 -0700

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// If you are compliling this in Microsoft Visual Studio Code, use this command: gcc -g -o "name your program" "name of c program"
+// Example: gcc -g -o helloworld helloworld.c
+
 int main()
 
 {
